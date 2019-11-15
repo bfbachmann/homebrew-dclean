d939371503c1b453ca8c5aa35397020f8b08343d
Bruno Bachmann <bfbachmann@gmail.com>
2019-11-15 00:09:51 -0800

    Release v0.2.0

--- Formula/dclean.rb
+++ Formula/dclean.rb
@@ -1,8 +1,8 @@
 class Dclean < Formula
     desc "A command line utility for deleting old versions of Docker images."
     homepage "https://github.com/bfbachmann/dclean"
-    url "https://github.com/bfbachmann/dclean/releases/download/0.1.0/dclean-mac.tar.gz"
-    sha256 "ce199748185b32dbe17194bfe36db71487e3bb2504a0f5262050ae2122b3e962"
+    url "https://github.com/bfbachmann/dclean/releases/download/0.2.0/dclean-mac.tar.gz"
+    sha256 "5697261a4a71e017b86d440bf32f17ee0c338384ed04e3aa890e7d44613690a6"
     version "0.1.0"
 
     def install
